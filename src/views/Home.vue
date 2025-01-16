5f7b6b67f7633b953bf0a49c9e1ecd2fc10a4bc0
ebor94 <ebor94@hotmail.com>
2025-01-16 10:53:43 -0500

    add home component

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <TopBar />
+      <Headers />
+      <main>
+        <slot></slot>
+      </main>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import TopBar from '../components/TopBar.vue'
+  import Headers from '../components/Header.vue'
+ </script>
