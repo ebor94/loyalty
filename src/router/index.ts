d9fc1b238981690a460610c0100cffc26aced866
ebor94 <ebor94@hotmail.com>
2025-01-16 10:51:51 -0500

    add component home

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ProfessionalConfirmation from '../views/ProfessionalConfirmation.vue'
 import ClientConfirmation from '../views/ClientConfirmation.vue'
 import ProfessionalDashboard from '../views/ProfessionalDashboard.vue'
+import Home from '../views/Home.vue'
 
 const routes = [
+  {
+    path: '/',
+    component: Home
+  },
   {
     path: '/client/:id',
     component: ClientConfirmation
