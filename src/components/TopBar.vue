a103d1a956cb7d1cf31912022a6afaba930b5dcd
ebor94 <ebor94@hotmail.com>
2025-01-16 10:53:08 -0500

    add topbar component

---
+++ src/components/TopBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-gray-100 py-1">
+      <div class="max-w-7xl mx-auto px-4 flex justify-between items-center text-sm">
+        <div>
+          ¡Inicia sesión y empieza a ganar!
+        </div>
+        <div class="flex gap-4">
+          <a href="#" class="hover:text-gray-600">Soporte</a>
+          <span>|</span>
+          <a href="#" class="hover:text-gray-600">Login</a>
+          <span>|</span>
+         </div>
+      </div>
+    </div>
+  </template>
