6eb7e7b0370e5f1ab96d6b8c3ab6fa29023f1ba6
ebor94 <ebor94@hotmail.com>
2025-01-24 09:40:05 -0500

    create component registro

---
+++ src/components/SlideRegistro.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="relative py-8 md:py-16 bg-black">
+    <div class="absolute inset-0">
+      <img
+        src="/src/assets/img/perfil.jpg"
+        alt="Background"
+        class="w-full h-full object-cover opacity-40"
+      />
+    </div>
+    <div class="relative max-w-7xl mx-auto px-4">
+      <div class="flex flex-col lg:flex-row items-center gap-8 lg:gap-12">
+        <!-- Texto -->
+        <div class="w-full lg:w-1/3 text-center lg:text-left">
+          <h2 class="text-3xl md:text-5xl font-bold text-white leading-tight">
+           Registro       
+          </h2>
+        </div>        
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ src/views/Regsitro.vue
@@ -0,0 +1,11 @@
+<template>
+     <TopBar />
+      <Header></Header>
+      <SlideRegistro></SlideRegistro>
+      <Footer></Footer>
+</template>
+<script setup lang="ts">
+    import TopBar from '../components/TopBar.vue'
+    import Header from '../components/Header.vue'
+    import SlideRegistro from '../components/SlideRegistro.vue'
+</script>
