a7cb286982be4f82fd132be2ef4cbffd8139f7de
ebor94 <ebor94@hotmail.com>
2025-01-24 09:38:41 -0500

    add parametres slider home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   
       <TopBar />
-      <Header></Header>
+        <Header></Header>
+
+      <Slider pathSlider="./src/assets/img/pagina-home.jpg"></Slider>
       <HeroSlider />
       <ExperienceSection />
       <AboutSection/>
@@ -13,14 +15,17 @@
   </template>
   
   <script setup lang="ts">
+
   import TopBar from '../components/TopBar.vue'
   import Header from '../components/Header.vue'
-  import HeroSlider from '../components/Slider.vue'
+    // @ts-ignore
+  import Slider from '../components/Slider.vue'
   import ExperienceSection from '../components/experience.vue'
   import AboutSection from '../components/about.vue'
   import Brands from '../components/brands.vue'
   import LatestCupon from '../components/LatestCupon.vue'
   import CallToAction from '../components/CallToAction.vue'
+    // @ts-ignore
   import Testimonial from '../components/Testimonial.vue'
   import Footer from '../components/Footer.vue'
  </script>
