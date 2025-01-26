86111dacc7cd1860b1fa7296df2fc4327ccae52e
ebor94 <ebor94@hotmail.com>
2025-01-26 10:30:32 -0500

    add div solution error footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,32 @@
 <template>
-  
+    <div>
       <TopBar />
-        <Header></Header>
-
+      <Header></Header>
       <Slider pathSlider="./src/assets/img/pagina-home.jpg"></Slider>
       <HeroSlider />
       <ExperienceSection />
-      <AboutSection/>
+      <about/>
       <Brands />
       <LatestCupon />
       <CallToAction />
       <Testimonial></Testimonial>
       <Footer></Footer>
+    </div>
   </template>
   
   <script setup lang="ts">
-
+// @ts-ignore
   import TopBar from '../components/TopBar.vue'
+  // @ts-ignore
   import Header from '../components/Header.vue'
     // @ts-ignore
   import Slider from '../components/Slider.vue'
   import ExperienceSection from '../components/experience.vue'
-  import AboutSection from '../components/about.vue'
+  // @ts-ignore
+  import about from '../components/about.vue'
   import Brands from '../components/brands.vue'
   import LatestCupon from '../components/LatestCupon.vue'
+  // @ts-ignore
   import CallToAction from '../components/CallToAction.vue'
     // @ts-ignore
   import Testimonial from '../components/Testimonial.vue'
