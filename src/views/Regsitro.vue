d91697c40956ede70abbf5f08a50fc42b6793a84
ebor94 <ebor94@hotmail.com>
2025-01-27 14:14:02 -0500

    add events login and registro

--- src/views/Regsitro.vue
+++ src/views/Regsitro.vue
@@ -1,17 +1,28 @@
 <template>
     <div>
-     <TopBar />
-      <Header></Header>
-      <SlideRegistro></SlideRegistro>
-      <Registro></Registro>      
-      <Footer></Footer>
+        <TopBar />
+        <Header></Header>
+        <SlideRegistro></SlideRegistro>
+        <Login    v-if="mostrarComponente === 'login'" @mostrarRegistro="cambiarComponente('registro')" ></Login>
+        <Registro v-if="mostrarComponente === 'registro'" @mostrarLogin="cambiarComponente('login')" ></Registro>
+        <Footer></Footer>
     </div>
 </template>
 <script setup lang="ts">
-    import TopBar from '../components/TopBar.vue'
- //@ts-ignore
-    import Header from '../components/Header.vue'
-    import SlideRegistro from '../components/SlideRegistro.vue'
-    //@ts-ignore
-    import Registro from '../components/Registro.vue';
+//@ts-ignore
+import TopBar from '../components/TopBar.vue'
+//@ts-ignore
+import Header from '../components/Header.vue'
+import SlideRegistro from '../components/SlideRegistro.vue'
+//@ts-ignore
+import Registro from '../components/Registro.vue';
+//@ts-ignore
+import Login from '../components/Login.vue';
+import {ref} from 'vue';
+const mostrarComponente = ref<'login' | 'registro'>('login'); 
+const cambiarComponente = (componente: 'login' | 'registro') => {
+  mostrarComponente.value = componente;
+};
+
+
 </script>
