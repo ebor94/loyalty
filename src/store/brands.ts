1e408a87187044f0b882c4452cb783fabe908c15
ebor94 <ebor94@hotmail.com>
2025-01-27 16:39:24 -0500

    add categories store

--- src/store/brands.ts
+++ src/store/brands.ts
@@ -6,10 +6,15 @@ interface Store {
  id: number
  empresa: string
  logoWallet: string
+ categoria: string
 }
+ interface Categories {
+  name: string
+ }
 
 export const useStoreStore = defineStore('store', () => {
  const stores = ref<Store[]>([])
+ const categories = ref<Categories[]>([])
  const loading = ref(false)
  const error = ref<string | null>(null)
 
@@ -21,6 +26,10 @@ export const useStoreStore = defineStore('store', () => {
      const response = await fetch("https://lilix.ceramicaitalia.com:3001/clientes/italpuntos/CuponList", {method: "POST", redirect: "follow"})
      let  brands = await response.json();
      stores.value = brands.data.data ;
+     // Obtener categorías únicas
+     const nombresCategorias = stores.value.map(store => store.categoria);
+     const categoriasUnicas = [...new Set(nombresCategorias)].map(nombre => ({ name: nombre }));
+     categories.value = categoriasUnicas;
    } catch (e) {
      error.value = 'Error loading stores'
    } finally {
@@ -32,6 +41,7 @@ export const useStoreStore = defineStore('store', () => {
    stores,
    loading, 
    error,
+   categories,
    fetchStores
  }
 })
