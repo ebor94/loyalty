a9c2e6e44e6dc38b79c20f858ae1c994a674a97d
ebor94 <ebor94@hotmail.com>
2025-01-27 16:41:29 -0500

    add store component

---
+++ src/views/Store.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <TopBar  @mostrarLogin="toggleMostrarLogin" />
+    <Header></Header>
+    <Footer></Footer>
+    </div>
+</template>
+<script lang="ts" setup>
+import {ref} from 'vue';
+import Footer from '../components/Footer.vue';
+ // @ts-ignore
+import Header from '../components/Header.vue';
+ // @ts-ignore
+import TopBar from '../components/TopBar.vue';
+
+const mostrarLogin = ref(false);
+const mostrarRegistro = ref(false);
+const toggleMostrarLogin = () => {
+  mostrarLogin.value = !mostrarLogin.value;
+  mostrarRegistro.value = false; // Ocultar registro si se muestra login
+
+};
+</script>
