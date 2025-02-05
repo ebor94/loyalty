269db638804342a12200ecb36244e6770c740487
ebor94 <ebor94@hotmail.com>
2025-02-05 15:05:36 -0500

    add api for get img

--- src/service/cupon.ts
+++ src/service/cupon.ts
@@ -2,31 +2,45 @@ import axios from "axios";
 const domain = "https://lilix.ceramicaitalia.com:3001";
 
 export const Cupon = {
-    async getGifCard(id: string): Promise<any> {
-  
-      try {
+  async getGifCard(id: string): Promise<any> {
+    try {
+      let data = JSON.stringify({
+        id: id,
+      });
 
-        let data = JSON.stringify({
-            "id": id
-          });
-          
-        const response = await axios({
-          method: "post",
-          maxBodyLength: Infinity,
-          url: `${domain}/clientes/italpuntos/getGiftCard/`,
-          headers: {
-            "Content-Type": "application/json",
-            },
-            data : data
-        });
-       
-        return response.data;
-      } catch (error) {
-        console.log(error);
-        return null;
-      }
+      const response = await axios({
+        method: "post",
+        maxBodyLength: Infinity,
+        url: `${domain}/clientes/italpuntos/getGiftCard/`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: data,
+      });
+
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return null;
     }
-  };
+  },
 
+  async getImg(id: number, category: string): Promise<any> {
+    try {
+      let  response = await axios({
+        method: "get",
+        maxBodyLength: Infinity,
+        url: `https://web.ceramicaitalia.com/temporada/italpuntos/${category}/${id}.jpg`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
+      return response.status;
+    } catch (error) {
+      return 500;
+    }
 
+    
+  },
+};
