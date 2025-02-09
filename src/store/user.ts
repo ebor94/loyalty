df5c20cadc88b24d8f21322c0b16c11f3b65dab2
ebor94 <ebor94@hotmail.com>
2025-02-09 13:15:02 -0500

    add getters for validate session and save data user when login succes

--- src/store/user.ts
+++ src/store/user.ts
@@ -3,20 +3,39 @@ import { defineStore } from 'pinia';
 interface UserState {
   token: string | null;
   TokenSession: string | null;
+  userCode: string | null;
+  bpCode: string | null;
+  userName: string | null;
 }
 
 export const useUserStore = defineStore('user', {
   state: (): UserState => ({
     token: null,
     TokenSession: null,
+    userCode: null,
+    bpCode: null,
+    userName: null,
   }),
+
+  getters: {
+    isAuthenticated(state): boolean {
+      return !!state.TokenSession; // Verifica si hay un TokenSession
+    },
+  },
+
   actions: {
-    setToken(token: string) {
+    setToken(token: string, TokenSession: string) {
       this.token = token;
+      this.TokenSession = TokenSession;
     },
     clearToken() {
       this.token = null;
       this.TokenSession = null;
-    }
-  }
+    },
+    saveUser(userName: string, userCode: string, bpCode: string) {
+      this.userName = userName;
+      this.bpCode = bpCode;
+      this.userCode = userCode;
+    },
+  },
 });
