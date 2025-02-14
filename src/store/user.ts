cd5356e67608ece83b24d9a8d68aacc86ca03569
ebor94 <ebor94@hotmail.com>
2025-02-14 09:30:31 -0500

    add function for get info balance user

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,4 +1,6 @@
 import { defineStore } from 'pinia';
+import { cliente } from '../service/clientes';
+import { Cupon } from '../service/cupon';
 
 interface UserState {
   token: string | null;
@@ -9,6 +11,8 @@ interface UserState {
   puntosAcumulados: number;
   puntosDisponibles: number;
   PuntosRedimidos : number;
+  Acumulaciones : Array<any>;
+  Redenciones : Array<any>;
 }
 
 export const useUserStore = defineStore('user', {
@@ -21,6 +25,8 @@ export const useUserStore = defineStore('user', {
     puntosAcumulados : 0,
     puntosDisponibles : 0,
     PuntosRedimidos : 0,
+    Acumulaciones : [],
+    Redenciones: [],	
   }),
   
 
@@ -43,12 +49,23 @@ export const useUserStore = defineStore('user', {
       this.userName = userName;
       this.bpCode = bpCode;
       this.userCode = userCode;
+     this.getDataUser(this.userCode, this.bpCode);
+      
     },
-    getDataUser(usercode: string, bpCode: string){
+    async getDataUser(usercode: string, bpCode: string){
+      const data = await cliente.getDataloyalty(usercode,'','14',usercode,0)
+      const balance = await Cupon.getGiftcardBought(usercode);
+      this.Acumulaciones =  data.filter((item:any) => item.aprobcte === '1')      
+      this.Redenciones = balance.data;
+      this.PuntosRedimidos =  this.Redenciones.reduce((sum, item) => sum + item.valor, 0);
+      this.puntosAcumulados = data.reduce((sum : number, item : any) =>  item.aprobcte === '1' ? sum + item.margenaliado : sum, 0);
+      this.puntosDisponibles = this.puntosAcumulados - this.PuntosRedimidos
+      this.puntosDisponibles = this.puntosDisponibles < 0 ? 0 : this.puntosDisponibles
+         
 
     },
     updateDataUser(usercode: string, bpCode: string, puntosRedimidos: number) {
-
+      this.puntosDisponibles = this.puntosDisponibles - puntosRedimidos
     }
   },
 });
