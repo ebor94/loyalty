108b20ab6aa356ff85a953a61ea8e011ee3eb9b7
ebor94 <ebor94@hotmail.com>
2025-02-14 09:16:18 -0500

    add function click iniciar sesion and background-color

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="bg-gray-100 py-1">
-      <div class="max-w-7xl mx-auto px-4 flex justify-between items-center text-sm">
+    <div class="bg-red-800 py-1">
+      <div class="max-w-7xl mx-auto px-4 flex justify-between items-center text-sm text-white">
         <div>
           ¡Inicia sesión y empieza a ganar!
         </div>
@@ -17,8 +17,10 @@
   <script setup lang="tsx">
 import { ref, computed } from 'vue';
 import { useUserStore } from '../store/user'; // Ajusta la ruta a tu store
+import { useRouter } from 'vue-router';
 
 const userStore = useUserStore();
+const router = useRouter()
 
 // Puedes usar un computed property para determinar si el usuario está logueado
 const usuarioLogueado = computed(() => userStore.token !== null); 
@@ -30,6 +32,9 @@ const mostrarFormularioLogin = () => {
   // Si el usuario está logueado, cerrar sesión
   if (usuarioLogueado.value) {
     userStore.clearToken();
+    router.push('/')
+  }else{
+    router.push('/registro')
   }
 };
 
