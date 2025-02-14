26401033722a04970578f923a7e265dba21546c9
ebor94 <ebor94@hotmail.com>
2025-02-14 09:43:47 -0500

    cnhage names componente capitalize

--- src/views/Regsitro.vue
+++ src/views/Regsitro.vue
@@ -2,9 +2,9 @@
     <div>
         <TopBar />
         <Header></Header>
-        <SlideRegistro></SlideRegistro>
-        <Login    v-if="mostrarComponente === 'login'" @mostrarRegistro="cambiarComponente('registro')" ></Login>
-        <Registro v-if="mostrarComponente === 'registro'" @mostrarLogin="cambiarComponente('login')" ></Registro>
+        <SlideRegistro :Titulo="mostrarComponente"></SlideRegistro>
+        <Login    v-if="mostrarComponente === 'login'" @mostrarRegistro="cambiarComponente('Registro')" ></Login>
+        <Registro v-if="mostrarComponente === 'Registro'" @mostrarLogin="cambiarComponente('login')" ></Registro>
         <Footer></Footer>
     </div>
 </template>
@@ -13,14 +13,15 @@
 import TopBar from '../components/TopBar.vue'
 //@ts-ignore
 import Header from '../components/Header.vue'
+//@ts-ignore
 import SlideRegistro from '../components/SlideRegistro.vue'
 //@ts-ignore
 import Registro from '../components/Registro.vue';
 //@ts-ignore
 import Login from '../components/Login.vue';
 import {ref} from 'vue';
-const mostrarComponente = ref<'login' | 'registro'>('login'); 
-const cambiarComponente = (componente: 'login' | 'registro') => {
+const mostrarComponente = ref<'login' | 'Registro'>('login'); 
+const cambiarComponente = (componente: 'login' | 'Registro') => {
   mostrarComponente.value = componente;
 };
 
