a52a3001a1e97f6b9e8cc985a8470ae9515f5b80
ebor94 <ebor94@hotmail.com>
2025-02-14 09:23:20 -0500

    add functions for get data user info and save redemptions

--- src/service/clientes.ts
+++ src/service/clientes.ts
@@ -12,9 +12,9 @@ export const cliente = {
           "Content-Type": "application/json",
           Authorization: "Basic Ym9ydGVnYVpCb3J0ZWdhKzExM==",
         },
-        data: JSON.stringify({ cc })
+        data: JSON.stringify({ cc }),
       });
-      
+
       return response.data;
     } catch (error) {
       console.log(error);
@@ -22,24 +22,85 @@ export const cliente = {
     }
   },
 
-  async getTokenSession(userCode: string | null ,bpCode : string | null): Promise<any> {
+  async getTokenSession(
+    userCode: string | null,
+    bpCode: string | null
+  ): Promise<any> {
     try {
-      const  response = await axios({
+      const response = await axios({
         method: "post",
         maxBodyLength: Infinity,
         url: `${domain}/auth/login/italpuntos/`,
         headers: {
           "Content-Type": "application/json",
-      
         },
-        data: JSON.stringify({ userCode, bpCode })
-
-      })
+        data: JSON.stringify({ userCode, bpCode }),
+      });
 
       return response.data;
-      
     } catch (error) {
       return 500;
     }
-  }
+  },
+
+  async getDataloyalty(
+    cc: string,
+    pedido: string,
+    BANDERA: string,
+    FILTRO: string,
+    MARGENALIADO: number
+  ): Promise<any> {
+    let data: object = {
+      BANDERA: BANDERA,
+      OFERTA: pedido,
+      MARGENALIADO: MARGENALIADO,
+      MARGENINTERNO: "",
+      TOKEN: "",
+      RESPUESTAWP: "",
+      USUARIOAPROB: "",
+      FILTRO: FILTRO,
+      CODBP: cc,
+    };
+    try {
+      const response = await axios({
+        method: "post",
+        maxBodyLength: Infinity,
+        url: `${domain}/clientes/quote/`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify(data),
+      });
+
+      return response.data;
+    } catch (error) {
+      return JSON.stringify(error);
+    }
+  },
+  async registraAprobacion(
+    consecutivo: string,
+    pedido: string,
+    codBp: string
+  ): Promise<any> {
+    try {
+      let data: object = {
+        consecutivo: consecutivo,
+        pedido: pedido,
+        codBp: codBp,
+      };
+
+      const response = await axios({
+        method: "post",
+        maxBodyLength: Infinity,
+        url: `${domain}/clientes/italpuntos/registrar`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify(data),
+      });
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  },
 };
